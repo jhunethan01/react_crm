597d5127a82c0d09295a3cdbddcaf537f4805507
jhunethan01 <jhunethan23@gmail.com>
2023-12-24 16:08:08 +0000

    adds tables for customers and results

--- src/Components/Customers.tsx
+++ src/Components/Customers.tsx
@@ -4,8 +4,54 @@ import '../css/Customers.css';
 
 function Customers() {
   return (
-    <section>
+    <section className='dashboard-container'>
+      <h1 className='tab-title'>
         Customers
+      </h1>
+      <p>
+        Here you can view and manage all customers in the system. You can add new customers, edit existing ones or delete them if necessary.
+      </p>
+
+      <table className='striped-table'>
+        <thead>
+          <tr>
+            <td>
+              First Name
+            </td>
+            <td>
+              Last Name
+            </td>
+            <td>
+              Email
+            </td>
+            <td>
+              Number of courses
+            </td>
+            <td>
+              Created
+            </td>
+          </tr>
+
+        </thead>
+        <tbody>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+            <td>Data 3</td>
+            <td>Data 4</td>
+            <td>Data 5</td>
+          </tr>
+          <tr>
+            <td>Data 4</td>
+            <td>Data 5</td>
+            <td>Data 6</td>
+            <td>Data 7</td>
+            <td>Data 8</td>
+          </tr>
+        </tbody>
+
+      </table>
+
     </section>
   );
 }
--- src/Components/Results.tsx
+++ src/Components/Results.tsx
@@ -4,9 +4,56 @@ import '../css/Results.css';
 
 function Results() {
   return (
-    <section>
+    <section className='dashboard-container'>
+      <h1 className='tab-title'>
         Results
+      </h1>
+      <p>
+        This is where you will see the results of your searches for test results. You can filter these results by clicking on the controls below.
+      </p>
+
+      <table className='striped-table'>
+        <thead>
+          <tr>
+            <td>
+              Course ID
+            </td>
+            <td>
+              First Name
+            </td>
+            <td>
+              Last Name
+            </td>
+            <td>
+              Score
+            </td>
+            <td>
+              Date Completed
+            </td>
+          </tr>
+
+        </thead>
+        <tbody>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+            <td>Data 3</td>
+            <td>Data 4</td>
+            <td>Data 5</td>
+          </tr>
+          <tr>
+            <td>Data 4</td>
+            <td>Data 5</td>
+            <td>Data 6</td>
+            <td>Data 7</td>
+            <td>Data 8</td>
+          </tr>
+        </tbody>
+
+      </table>
+
     </section>
+
   );
 }
 
