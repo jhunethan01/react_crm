136939756b6c54f5ee55b2326f686596ce2d7ca3
Ethan Lay <jhunethan23@gmail.com>
2024-04-11 11:14:29 +0100

    small fix

--- src/Components/Overview/Overview.tsx
+++ src/Components/Overview/Overview.tsx
@@ -10,7 +10,7 @@ function Overview({ customerList, resultsList }) {
 
   const dateFrequencyMap = {};
 
-  for (const obj of resultsList.results) {
+  for (const obj of resultsList) {
     const date = obj.date.slice(0, 10); 
     if (!dateFrequencyMap[date]) dateFrequencyMap[date] = 1;
     else dateFrequencyMap[date]++;
@@ -23,10 +23,10 @@ function Overview({ customerList, resultsList }) {
     <section className='dashboard-container'>
       <Welcome title={'Overview'} description={'Welcome to the dashboard! Here you can view important information about your account and manage your tasks. To get started, select a tab to the left.'} />
       <div className="dashboard_statistic-container">
-        <Statistic title={'Number of Customers'} number={customerList.customers.length} />
-        <Statistic title={'Total Courses Completed'} number={resultsList.results.length} />
+        <Statistic title={'Number of Customers'} number={customerList.length} />
+        <Statistic title={'Total Courses Completed'} number={resultsList.length} />
         <div className='line-break' />
-        <ResultsHistory results={resultsList.results} />
+        <ResultsHistory results={resultsList} />
         <Chart dataX={dataX} dataY={dataY} />
       </div>
     </section>
